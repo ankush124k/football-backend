c1b457187b98b17955c11b5ec1c51c7f711a64ab
Ankush Kumar Kushwaha <ankush@radiantinfonet.com>
2025-01-24 02:22:41 +0530

    user schema added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,41 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
+    UserName:{type:String,required:true},
     email:{type:String,required:true,unique:true},
-    hashedpassword:{type:String},
+    password:{type:String},
     isVerified:{type:Boolean,default:false},
     verificationCode: { type: String }, 
     verificationExpiry: { type: Date },
+    refereshToken:{type:String}
 }, {timestamps: true});
 
-export const User=mongoose.model('User',userSchema);
+userSchema.pre("save",async function (next) {
+    if(this.isModified("password")) return next();
+
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function (password) {
+    return  await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+       { 
+        _id:this._id,
+        email:this.email,
+        userName:this.userName,
+       },
+       process.env.ACCESS_TOKEN_SECRET,
+       {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+       }
+
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
